44adc038880d195d277938630f1406b6b105ccb4
Priyaa-Agrawal <priyajuly1907@gmail.com>
2020-04-27 01:34:59 +0530

    graph

--- senti/sentianalysis.py
+++ senti/sentianalysis.py
@@ -4,12 +4,12 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
 
 
 # Analysing Sentiment as Positve or Negative or Neutral
 def sentiment_analyse(sentiment_text):
     score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
-
     neg = score['neg']
     pos = score['pos']
     if neg > pos:
@@ -31,5 +31,43 @@ def main_function(text):
 
     # Calling sentiment analyse function
     temp = sentiment_analyse(cleaned_text)
+
+    # Graph Ploting Process
+
+    # Converting text into token list
+    tokenized_words = word_tokenize(cleaned_text, "english")
+
+    # Removing Stopwords from the tokenized words
+    final_words = []
+    for word in tokenized_words:
+        if word not in stopwords.words('english'):
+            final_words.append(word)
+
+    # Calculating Emotion of the final words
+    emotion_list = []
+    with open('senti/emotions.txt', 'r') as file:
+        for line in file:
+            clear_line = line.replace("\n", '').replace(
+                ",", '').replace("'", '').strip()
+            word, emotion = clear_line.split(':')
+            if word in final_words:
+                emotion_list.append(emotion)
+
+    # Counting words of same emotions
+    w = Counter(emotion_list)
+
+    # Bar Graph
+    fig, ax1 = plt.subplots()
+    ax1.bar(w.keys(), w.values())
+    fig.autofmt_xdate()
+    plt.savefig('media/graph.png')
+    plt.close()
+
+    # WordCloud graph
+    stop_words = set(STOPWORDS)
+    wordcloud = WordCloud(width=800, height=800, background_color='white',
+                          stopwords=stop_words, min_font_size=10).generate(text)
+
+    wordcloud.to_file('media/cloud.png')
 
     return temp
