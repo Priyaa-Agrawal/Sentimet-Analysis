b62a549d6e0157649c2ef821abb079b6e6fc407f
Priyaa-Agrawal <priyajuly1907@gmail.com>
2020-06-28 13:13:33 +0530

    changes

--- senti/views.py
+++ senti/views.py
@@ -15,27 +15,31 @@ def textanalysis(request):
     input_data = request.POST.get('input_data')
     filename = request.FILES.get('filename')
     # print(filename)
-    if filename is not None:
-        file_ext = os.path.splitext(filename.name)[1]
-        fname = datetime.datetime.now().strftime("%y%m%d%H%M%S")
 
-        # print(fname, file_ext)
-        src = f'media/input/{fname}{file_ext}'
-        with open(src, 'wb+') as f:
-            for chunk in filename.chunks():
-                f.write(chunk)
-        x = ipf.all_file(src)
-    else:
-        x = input_data
-    data = main_function(x)
-    
-    context = {
-        'text': x,
-        'sentiment': data[0],
-        'graph': data[1][0],
-        'cloud': data[1][1]
-    }
-    return render(request, 'senti/textresult.html', context)
+    try:
+        if filename is not None:
+            file_ext = os.path.splitext(filename.name)[1]
+            fname = datetime.datetime.now().strftime("%y%m%d%H%M%S")
+
+            # print(fname, file_ext)
+            src = f'media/input/{fname}{file_ext}'
+            with open(src, 'wb+') as f:
+                for chunk in filename.chunks():
+                    f.write(chunk)
+            x = ipf.all_file(src)
+        else:
+            x = input_data
+        data = main_function(x)
+        
+        context = {
+            'text': x,
+            'sentiment': data[0],
+            'graph': data[1][0],
+            'cloud': data[1][1]
+        }
+        return render(request, 'senti/textresult.html', context)
+    except ValueError as identifier:
+        pass
 
 def tweetanalysis(request):
     tweet = request.POST.get('tweet')
